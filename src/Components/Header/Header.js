db96a8d118ad9d3d91eb1b0183fb56e3bd39ef0f
proloypaul <proloyp23@gmail.com>
2021-12-14 18:06:30 +0600

    set navigation

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import { Container } from '@mui/material';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import "./Header.css";
 
 const Header = () => {
+    const [showBar, setShowBar] = useState(false)
+    
     return (
         <div className='navbar-container'>
-            <nav>
-                <div className='nav-logo'>
-                    <h1>lkfjldkjflkdjfkdjfdkfj</h1>
-
+            <Container>
+                <div className='navbar'>
+                    <div className='nav-title'>
+                        <h1>EklashPurHighSchool</h1>
+                    </div>
+                    <p className='bar-button' onClick={() => setShowBar(!showBar) }><i className="fas fa-bars"></i></p>
+                    <div className="nav-links">
+                        <ul>
+                            <li><Link to="/home">Home</Link></li>
+                            <li><Link to="/institute">Institute</Link></li>
+                            <li><Link to="/teachers">Teachers</Link></li>
+                            <li><Link to="/students">Students</Link></li>
+                            <li><Link to="/notice">Notice</Link></li>
+                        </ul>
+                        
+                    </div>
                 </div>
-                <div className='nav-bar'>
-                    <Link to="/home">Home</Link>
-                    <Link to="/institute">Institute</Link>
-                    <Link to="/teachers">Teachers</Link>
-                    <Link to="/students">Students</Link>
-                    <Link to="/notice">Notice</Link>
-                </div>
-            </nav>
+            </Container>
         </div>
     );
 };
