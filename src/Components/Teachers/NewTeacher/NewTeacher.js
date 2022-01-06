90e0bed5ffb11ca6512e39ea036778278631599d
proloypaul <proloyp23@gmail.com>
2022-01-06 23:43:53 +0600

    added new teacher form

--- src/Components/Teachers/NewTeacher/NewTeacher.js
+++ src/Components/Teachers/NewTeacher/NewTeacher.js
@@ -1,11 +1,37 @@
 import { Container } from '@mui/material';
 import React from 'react';
-
+import './NewTeacher.css';
 const NewTeacher = () => {
     return (
-        <div>
+        <div className='newTeacherContainer'>
             <Container sx={{py: 10, textAlign:"center"}}>
-                <h1>New Teachers Comming Soon......</h1>
+                <h1>Welcome Teacher Of Our School</h1>
+                <div>
+                    <from className="teachersFrom">
+                        <div className='commonFrom'>
+                            <input type="text" placeholder="Teacher Name"/>
+                            <input type="text" placeholder="Position"/>
+                        </div>
+                        <div className='commonFrom'>
+                            <input type="text" placeholder="Subject Name"/>
+                            <input type="text" placeholder="Education"/>
+                        </div>
+                        <div className='commonFrom'>
+                            <input type="text" placeholder="Classes"/>
+                            <input type="text" placeholder="Day"/>
+                        </div>
+                        <div className='commonFrom'>
+                            <input type="text" placeholder="Time"/>
+                            <input type="text" placeholder="Salary"/>
+                        </div>
+                        <div className='teacherImg'>
+                            <input type="text" placeholder="Teacher Img host link"/>
+                        </div>
+                        <div className='teacherAbout'>
+                            <textarea placeholder='Describe about teacher'></textarea>
+                        </div>
+                    </from>
+                </div>
             </Container>
         </div>
     );
