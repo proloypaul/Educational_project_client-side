10a6aa99b0ed0788fae4e14a06c94b17dce9ad08
proloypaul <proloyp23@gmail.com>
2022-01-14 12:23:07 -0800

    new teacher form modify

--- src/Components/Teachers/NewTeacher/NewTeacher.js
+++ src/Components/Teachers/NewTeacher/NewTeacher.js
@@ -1,34 +1,63 @@
 import { Container } from '@mui/material';
-import React from 'react';
+import React, {useState} from 'react';
 import './NewTeacher.css';
 const NewTeacher = () => {
+    const [teacherData, SetTeacherData] = useState({})
+
+    const handleText = event => {
+        const name = event.target.name
+        const filed = event.target.value
+        let allTeacherData = {...teacherData} 
+        allTeacherData[name] = filed;
+        SetTeacherData(allTeacherData)
+
+    }
+    const hanldeNewTeacher = event => {
+        event.preventDefault()
+        console.log(teacherData)
+
+        // const url = ``
+        // fetch('url', {
+        //     method: 'POST',
+        //     headers: {
+        //         'content-type': 'application/json'
+        //     },
+        //     body: JSON.stringify(teacherData)
+        // })
+
+    }
     return (
         <div className='newTeacherContainer'>
             <Container sx={{py: 10, textAlign:"center"}}>
-                <h1>Welcome Teacher Of Our School</h1>
+                <h1>Welcome Teacher Of Our institute</h1>
                 <div>
-                    <from className="teachersFrom">
+                    <from className="teachersFrom" onClick={hanldeNewTeacher}>
+                        <div>
                         <div className='commonFrom'>
-                            <input type="text" placeholder="Teacher Name"/>
-                            <input type="text" placeholder="Position"/>
+                            <input type="text" placeholder="Teacher Name" name='teacher' onBlur={handleText}/>
+                            <input type="text" placeholder="Position" name='position' onBlur={handleText}/>
                         </div>
                         <div className='commonFrom'>
-                            <input type="text" placeholder="Subject Name"/>
-                            <input type="text" placeholder="Education"/>
+                            <input type="text" placeholder="Subject Name" name='subject' onBlur={handleText}/>
+                            <input type="text" placeholder="Education" name='education' onBlur={handleText}/>
                         </div>
                         <div className='commonFrom'>
-                            <input type="text" placeholder="Classes"/>
-                            <input type="text" placeholder="Day"/>
+                            <input type="text" placeholder="Classes" name='class' onBlur={handleText}/>
+                            <input type="text" placeholder="Day" name='day' onBlur={handleText}/>
                         </div>
                         <div className='commonFrom'>
-                            <input type="text" placeholder="Time"/>
-                            <input type="text" placeholder="Salary"/>
+                            <input type="text" placeholder="Time" name='time' onBlur={handleText}/>
+                            <input type="text" placeholder="Salary" name='salary' onBlur={handleText}/>
                         </div>
                         <div className='teacherImg'>
-                            <input type="text" placeholder="Teacher Img host link"/>
+                            <input type="text" placeholder="Teacher Img host link" name='img' onBlur={handleText}/>
                         </div>
                         <div className='teacherAbout'>
-                            <textarea placeholder='Describe about teacher'></textarea>
+                            <textarea placeholder='Describe about teacher' name='description' onBlur={handleText}></textarea>
+                        </div>
+                        <div>
+                            <button type='submit' className='newTeacherBtn'>New Teacher</button>
+                        </div>
                         </div>
                     </from>
                 </div>
